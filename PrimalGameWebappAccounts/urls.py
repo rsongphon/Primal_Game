0332a85de755a7bb71a074f991f94367062a18d2
rsongphon <r.songphon@gmail.com>
2024-04-30 15:46:09 +0700

    create sign up page (without token)

--- PrimalGameWebappAccounts/urls.py
+++ PrimalGameWebappAccounts/urls.py
@@ -1,8 +1,11 @@
 from django.contrib.auth import views
 from django.urls import path
+from .views import SignUpView
 
+app_name = 'authen'
 
 urlpatterns = [
+    path("signup/", SignUpView.as_view(), name="signup"),
     path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
     path('password-reset/', views.PasswordResetView.as_view(), name='password_reset'),
